cb13afd4b637bd6ab6447c11e385c5dcc2b40380
BigsDev <33373842+BigsDev@users.noreply.github.com>
2020-05-02 20:18:12 +0300

    Add files via upload

---
+++ item_helmet.cpp
@@ -0,0 +1,45 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose: Handling for the suit batteries.
+//
+// $NoKeywords: $
+//=============================================================================//
+
+#include "cbase.h"
+#include "hl2_player.h"
+#include "basecombatweapon.h"
+#include "gamerules.h"
+#include "items.h"
+#include "engine/IEngineSound.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+class CItemHelmet : public CItem
+{
+public:
+	DECLARE_CLASS(CItemHelmet, CItem);
+
+	void Spawn(void)
+	{
+		Precache();
+		SetModel("models/items/car_battery01.mdl");
+		BaseClass::Spawn();
+	}
+	void Precache(void)
+	{
+		PrecacheModel("models/items/car_battery01.mdl");
+
+		PrecacheScriptSound("ItemBattery.Touch");
+
+	}
+	bool MyTouch(CBasePlayer *pPlayer)
+	{
+		CHL2_Player *pHL2Player = dynamic_cast<CHL2_Player *>(pPlayer);
+			return (pHL2Player && pHL2Player->ApplyHelmet());
+	}
+};
+
+LINK_ENTITY_TO_CLASS(item_helmet, CItemHelmet);
+PRECACHE_REGISTER(item_helmet);
+
---
+++ item_kevlar.cpp
@@ -0,0 +1,44 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose: Handling for the suit batteries.
+//
+// $NoKeywords: $
+//=============================================================================//
+
+#include "cbase.h"
+#include "hl2_player.h"
+#include "basecombatweapon.h"
+#include "gamerules.h"
+#include "items.h"
+#include "engine/IEngineSound.h"
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+class CItemKevlar : public CItem
+{	
+public:
+	DECLARE_CLASS(CItemKevlar, CItem);
+	void Spawn(void)
+	{
+		Precache();
+		SetModel("models/items/hevsuit.mdl");
+		BaseClass::Spawn();
+	}
+	void Precache(void)
+	{
+		PrecacheModel("models/items/hevsuit.mdl");
+
+		PrecacheScriptSound("ItemBattery.Touch");
+
+	}
+	bool MyTouch(CBasePlayer *pPlayer)
+	{
+		CHL2_Player *pHL2Player = dynamic_cast<CHL2_Player *>(pPlayer);
+		return (pHL2Player && pHL2Player->ApplyKevlar());
+	}
+
+};
+
+LINK_ENTITY_TO_CLASS(item_kevlar, CItemKevlar);
+PRECACHE_REGISTER(item_kevlar);
+
